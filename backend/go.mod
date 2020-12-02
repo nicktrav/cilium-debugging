1d4938385981d02f6a61b3eb70012a5c7b1629d8
Nick Travers <n.e.travers@gmail.com>
2020-12-02 12:35:46 -0800

    Various tweaks
    Add monitoring to the backend.
    
    CNP tweaks to allow Prometheus to scrape the backend pods.
    
    Update custom-metrics to use the "responses" metric.

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,8 @@
 module github.com/cloudnatix/cilium-custom-metrics-demo
 
 go 1.15
+
+require (
+	go.opentelemetry.io/otel v0.14.0
+	go.opentelemetry.io/otel/exporters/metric/prometheus v0.14.0
+)
--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/metric/prometheus"
+	"go.opentelemetry.io/otel/metric"
 )
 
 func main() {
@@ -15,8 +20,15 @@ func main() {
 		log.Fatal(err)
 	}
 
+	meter := otel.Meter("cloudnatix.com/backend")
+	echoRequests := metric.Must(meter).NewInt64Counter("echo.requests")
+	aRequests := metric.Must(meter).NewInt64Counter("a.requests")
+	bRequests := metric.Must(meter).NewInt64Counter("b.requests")
+
+	ctx := context.Background()
 	http.HandleFunc("/echo", func(writer http.ResponseWriter, request *http.Request) {
 		log.Printf("handling /echo request")
+		echoRequests.Add(ctx, 1)
 		b, err := ioutil.ReadAll(request.Body)
 		if err != nil {
 			log.Printf("read: %s", err)
@@ -27,14 +39,22 @@ func main() {
 	})
 
 	http.HandleFunc("/a", func(writer http.ResponseWriter, request *http.Request) {
+		aRequests.Add(ctx, 1)
 		log.Printf("handling /a request")
 		write(writer, fmt.Sprintf("Hello from endpoint /a (host=%s)", hostname))
 	})
 
 	http.HandleFunc("/b", func(writer http.ResponseWriter, request *http.Request) {
+		bRequests.Add(ctx, 1)
 		log.Printf("handling /b request")
 		write(writer, fmt.Sprintf("Hello from endpoint /b (host=%s)", hostname))
 	})
+
+	exp, err := prometheus.InstallNewPipeline(prometheus.Config{})
+	if err != nil {
+		log.Fatalf("failed to initialize metrics exporter: %s", err)
+	}
+	http.HandleFunc("/metrics", exp.ServeHTTP)
 
 	log.Printf("Starting server")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
